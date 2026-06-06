be743be6471d33f9a3c8293cc00e1dabd6e9456b
shinvigami <shinvigami@gmail.com>
2026-06-06 11:50:23 +0200

    Update pobieranie.py

--- src/pobieranie.py
+++ src/pobieranie.py
@@ -3,20 +3,35 @@ import requests
 import pandas as pd
 import os
 
+#Ta funkcja automatycznie pobiera dane z internetu
 def pobierz_dane():
-   url = "https://link.springer.com/article/10.1186/1744-9081-3-42/tables/2"
-  kod_strony = requests.get(url).text    #pobieranie strony
-soup = BeautifulSoup(kod_strony, "html.parser")
-
-tabela = soup.find("table")    #wyciąganie danych z tabeli
-wiersze = tabela.find_all("tr")
-
-wyniki = []
-for wiersz in wiersze:
-  komorki = wiersz.find_all(["td", "th"])
-  wyniki.append([k.text.strip() for k in komorki])
-
-df = pd.DataFrame(wyniki)      #zapis w folderze data
-os.makedirs("data", exist_ok=True)
-df.to_csv("data/tabela_dane.csv", index=False, header=False)
-print("Pobrano dane!")
+#Link do strony z badaniami nad ADHD
+    url = "https://link.springer.com/article/10.1186/1744-9081-3-42/tables/2"
+    
+   #Pobieramy kod strony jako zwykły tekst
+    kod_strony = requests.get(url).text
+    
+    #Narzędzie BeautifulSoup pomaga nam przeszukiwać kod strony
+    soup = BeautifulSoup(kod_strony, "html.parser")
+    
+    #Szukamy na stronie tabeli medycznej i jej wierszy
+    tabela = soup.find("table")
+    wiersze = tabela.find_all("tr")
+    
+    #Przepisujemy dane z tabeli internetowej do zwykłej listy w Pythonie
+    wyniki = []
+    for wiersz in wiersze:
+        komorki = wiersz.find_all(["td", "th"])
+        wyniki.append([k.text.strip() for k in komorki])
+        
+    #Zamieniamy listę na strukturę Pandas (DataFrame), żeby łatwiej robić statystyki
+    df = pd.DataFrame(wyniki)
+    
+    #Tworzymy folder o nazwie 'data' na dysku, jeśli jeszcze nie istnieje
+    os.makedirs("data", exist_ok=True)
+    
+    #Zapisujemy pobraną tabelę jako plik CSV w tym folderze
+    df.to_csv("data/tabela_dane.csv", index=False, header=False)
+    
+    #Informacja w terminalu, że wszystko się udało
+    print("Pobrano dane!")
