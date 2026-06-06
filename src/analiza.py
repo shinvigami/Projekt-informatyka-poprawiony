bea0bfecd2e00d724591e6392a4ccc968c009aca
shinvigami <shinvigami@gmail.com>
2026-06-06 11:57:04 +0200

    Update analiza.py

--- src/analiza.py
+++ src/analiza.py
@@ -2,18 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 #wczytanie danych z pliku
 def analizuj_dane():    
-df = pd.read_csv("data/tabela_dane.csv")
-#automatyczne liczenie statystyk opisowych np. średnia
-print(df.describe()) 
-#pierwszy wykres, słupkowy
-plt.figure()    
-df.iloc[1:5, 1].astype(float).plot(kind='bar')
-plt.savefig("data/wykres_1.png")
-plt.close()
-#drugi wykres, linowy
-plt.figure()    
-df.iloc[1:5, 2].astype(float).plot(kind='line')
-plt.savefig("data/wykres_2.png")
-plt.close()
+  df = pd.read_csv("data/tabela_dane.csv")
+  #automatyczne liczenie statystyk opisowych np. średnia
+  print(df.describe()) 
+  #pierwszy wykres, słupkowy
+  plt.figure()    
+  df.iloc[1:5, 1].astype(float).plot(kind='bar')
+  plt.savefig("data/wykres_1.png")
+  plt.close()
+  #drugi wykres, linowy
+  plt.figure()    
+  df.iloc[1:5, 2].astype(float).plot(kind='line')
+  plt.savefig("data/wykres_2.png")
+  plt.close()
 
-print("wykresy gotowe!")
+  print("wykresy gotowe!")
